c0f00e5f14c66af2fc5b23b57c7c39b51eb744a3
JanEspana <jan.espana.7e7@itb.cat>
2024-09-19 13:57:21 +0200

    Code updated

--- review_one1/Program.cs
+++ review_one1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        const string AVG = "El promig de les notes es: ", INTRO = "Introdueix la nota ", APPROVED = "Aprovat", NOTAPPROVED = "Suspès";
+        int sum = 0;
+        float avg;
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(INTRO + (i + 1));
+            sum += Convert.ToInt32(Console.ReadLine());
+        }
+        avg = sum / 3;
+
+        Console.WriteLine(AVG + avg);
+
+        if (avg >= 5)
+        {
+            Console.WriteLine(APPROVED);
+        }
+        else
+        {
+            Console.WriteLine(NOTAPPROVED);
+        }
+    }
+}
